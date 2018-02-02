7db6f035870555148211af6533b379930e98a773
shimma <shimma.dev@gmail.com>
2018-02-02 22:01:06 +0900

    update metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name                "opsworks-php7"
+description         "PHP7 for OpsWorks Ubuntu 14.04 LTS"
+long_description     IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer          "shimma"
+maintainer_email    "shimma.dev@gmail.com"
+license             "BSD"
+version             "0.0.1"
