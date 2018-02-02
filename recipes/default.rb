afe498121c4274119da97b3eb329db8dc75c17c8
shimma <shimma.dev@gmail.com>
2018-02-02 21:46:09 +0900

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,8 +12,21 @@ bash 'overrride install php7.2' do
     apt-get purge php5-fpm 
     apt-get --purge autoremove
 
-    apt install php7.2-fpm php7.2-mysql php7.2 php7.2-json php7.2-curl libapache2-mod-php7.2 php7.2-opcache php7.2-xml php7.2-gd php7.2-bz2 php7.2-mbstring libpcre3
-    service apache2 restart
+    apt install             \
+      php7.2                \
+      php7.2-mysql          \
+      php7.2-json           \
+      php7.2-curl           \
+      php7.2-opcache        \
+      php7.2-xml            \
+      php7.2-gd             \
+      php7.2-bz2            \
+      php7.2-mbstring       \
+      libapache2-mod-php7.2 \
+      libpcre3
+
+    a2dismod php5 || true
+    a2enmod php7.2
   EOH
   notifies :restart, "service[apache2]"
   not_if "which php7.2"
